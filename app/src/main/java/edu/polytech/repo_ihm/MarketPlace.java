935752ab9dde6bf8772b487716bd8d77c2a115e0
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2022-04-06 22:31:25 +0200

    MarketPlace Activity and Intent from homepage to MarketPlace

--- app/src/main/java/edu/polytech/repo_ihm/MarketPlace.java
+++ app/src/main/java/edu/polytech/repo_ihm/MarketPlace.java
@@ -2,7 +2,9 @@ package edu.polytech.repo_ihm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MarketPlace extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MarketPlace extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_place_hp);
+    }
+
+    public void returnHomePage(View view) {
+        Intent intent = new Intent(MarketPlace.this, MainActivity.class);
+        startActivity(intent);
     }
 }
