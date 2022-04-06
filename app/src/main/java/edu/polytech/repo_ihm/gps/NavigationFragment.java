5d498acb0f453d03acdab4ac4a28ee34bd4cd2b0
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2022-04-06 13:26:07 +0200

    GPS integration

---
+++ app/src/main/java/edu/polytech/repo_ihm/gps/NavigationFragment.java
@@ -0,0 +1,60 @@
+package edu.polytech.repo_ihm.gps;
+
+import static edu.polytech.repo_ihm.gps.IGPSActivity.REQUEST_CODE;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import edu.polytech.repo_ihm.R;
+
+public class NavigationFragment extends Fragment {
+
+    NavigationFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_navigation, container, false);
+        rootView.findViewById(R.id.buttonCheckPermission).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[] (Manifest.permission.ACCESS_COARSE_LOCATION), REQUEST_CODE);
+                }
+            }
+        });
+
+        rootView.findViewById(R.id.buttonSwitchOnOffGPS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getContext(), "You can change now", Toast.LENGTH_LONG);
+                toast.show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            }
+        });
+
+        rootView.findViewById(R.id.buttonSettings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getContext(), "Custom setting to this app", Toast.LENGTH_LONG);
+                toast.show();
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.fromParts("package", getActivity().getPackageName(), null));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        return rootView;
+    }
+}
