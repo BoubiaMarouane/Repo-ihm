c1b3e830d57489730da1fd60d13db4b2476310dd
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2022-04-06 22:11:49 +0200

    MarketPlace Activity and Intent from homepage to MarketPlace

--- app/src/main/java/edu/polytech/repo_ihm/MainActivity.java
+++ app/src/main/java/edu/polytech/repo_ihm/MainActivity.java
@@ -2,14 +2,27 @@ package edu.polytech.repo_ihm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.polytech.repo_ihm.gps.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    button =(Button) findViewById(R.id.btnMarketPlace);
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, MarketPlace.class);
+            startActivity(intent);
+        }
+    });
     }
 
 }
---
+++ app/src/main/java/edu/polytech/repo_ihm/MarketPlace.java
@@ -0,0 +1,14 @@
+package edu.polytech.repo_ihm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MarketPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_place_hp);
+    }
+}
