87e222700c88fbbd64b763f648ef5fa3021c8bad
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2022-04-14 19:39:41 +0200

    Implementing map and gps in market place

--- app/src/main/java/edu/polytech/repo_ihm/gps/NavigationFragment.java
+++ app/src/main/java/edu/polytech/repo_ihm/gps/NavigationFragment.java
@@ -14,46 +14,39 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import edu.polytech.repo_ihm.R;
 
 public class NavigationFragment extends Fragment {
+    public NavigationFragment() {
+    }
 
-    NavigationFragment(){}
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_navigation, container, false);
-        rootView.findViewById(R.id.buttonCheckPermission).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
-                }
+        rootView.findViewById(R.id.buttonCheckPermission).setOnClickListener(view -> {
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
             }
         });
-
-        rootView.findViewById(R.id.buttonSwitchOnOffGPS).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getContext(), "You can change now", Toast.LENGTH_LONG);
-                toast.show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            }
+        rootView.findViewById(R.id.buttonSwitchOnOffGPS).setOnClickListener(view -> {
+            Toast toast = Toast.makeText(getContext(), "You can Change Now", Toast.LENGTH_LONG);
+            toast.show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
         });
-
-        rootView.findViewById(R.id.buttonSettings).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getContext(), "Custom setting to this app", Toast.LENGTH_LONG);
-                toast.show();
-                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                        Uri.fromParts("package", getActivity().getPackageName(), null));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
+        rootView.findViewById(R.id.buttonSettings).setOnClickListener(view -> {
+            Toast toast = Toast.makeText(getContext(), "Custom setting to this app", Toast.LENGTH_LONG);
+            toast.show();
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", requireActivity().getPackageName(), null));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         });
         return rootView;
     }
