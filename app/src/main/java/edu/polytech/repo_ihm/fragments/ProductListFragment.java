5027817918a4829593f98c9cb440543012aa0b85
Arckene <thomas.farineau@icloud.com>
2022-04-17 18:33:18 +0200

    ...

--- app/src/main/java/edu/polytech/repo_ihm/fragments/ProductListFragment.java
+++ app/src/main/java/edu/polytech/repo_ihm/fragments/ProductListFragment.java
@@ -3,28 +3,18 @@ package edu.polytech.repo_ihm.fragments;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.text.MessageFormat;
-import java.util.Arrays;
+import androidx.fragment.app.Fragment;
+
 import java.util.List;
 import java.util.Locale;
 
@@ -53,9 +43,9 @@ public class ProductListFragment extends Fragment implements AdapterView.OnItemC
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             inventoryId = getArguments().getInt("IV_ID");
-            pName =  getActivity().findViewById(R.id.et_name_product);
-            pQty =   getActivity().findViewById(R.id.et_product_qty);
-            pDate =  getActivity().findViewById(R.id.et_product_date);
+            pName = getActivity().findViewById(R.id.et_name_product);
+            pQty = getActivity().findViewById(R.id.et_product_qty);
+            pDate = getActivity().findViewById(R.id.et_product_date);
             bSubmit = getActivity().findViewById(R.id.b_submit);
         }
 
@@ -76,7 +66,7 @@ public class ProductListFragment extends Fragment implements AdapterView.OnItemC
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         Product p = getIv(inventoryId).getProducts().get(i);
-        if(getIv(inventoryId).containProduct(p.getName())){
+        if (getIv(inventoryId).containProduct(p.getName())) {
             pName.setText(String.format("%s", p.getName()));
             pQty.setText(String.format("%s", p.getQuantity()));
             pDate.setText(String.format("%s", p.getDateP()));
@@ -88,60 +78,46 @@ public class ProductListFragment extends Fragment implements AdapterView.OnItemC
 
 
     //Impossible d'utiliser un attribut de Type Inventaire (fait crasher l'app) donc il faut utiliser le singleton à chaque fois...
-    private Inventory getIv(int id){
-        return InventoryList.getInstance().get(id);
+    private Inventory getIv(int id) {
+        return InventoryList.getInstance().getById(id);
     }
 }
 
 
+class ProductListAdapter extends BaseAdapter {
+    private final List<Product> productList;
+    private final Context context;
 
+    ProductListAdapter(Context context, List<Product> products) {
+        this.productList = products;
+        this.context = context;
+    }
 
-    /////////////////// Adapter ///////////////////
+    @Override
+    public int getCount() {
+        return productList.size();
+    }
 
+    @Override
+    public Object getItem(int i) {
+        return productList.get(i);
+    }
 
-    class ProductListAdapter extends BaseAdapter{
-        private List<Product> productList;
-        private LayoutInflater layoutInflater;
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
 
-        ProductListAdapter(Context context, List<Product> products) {
-            this.productList = products;
-            layoutInflater = LayoutInflater.from(context);
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        }
-
-         @Override
-        public int getCount() {
-            return productList.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return productList.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View convertView,  ViewGroup parent) {
-            ConstraintLayout layout;
-            if (convertView == null)
-                layout = (ConstraintLayout) layoutInflater.inflate(R.layout.custom_product_list_layout, parent, false);
-            else
-                layout = (ConstraintLayout) convertView;
-
-            ImageView img = layout.findViewById(R.id.img_product);
-            TextView pName = layout.findViewById(R.id.tv_product_name);
-            TextView pQty = layout.findViewById(R.id.tv_product_qty);
-            Product p = productList.get(i);
-            img.setImageResource(p.getImg());
-            pName.setText(p.getName());
-            pQty.setText(MessageFormat.format("qté: {0}", p.getQuantity()));
-
-            return layout;
-        }
+        @SuppressLint("ViewHolder") View view = layoutInflater.inflate(R.layout.product_list, parent, false);
+        TextView pName = view.findViewById(R.id.productName);
+        Product p = productList.get(i);
+        pName.setText(p.getName());
+        return view;
+    }
 }
 
 
