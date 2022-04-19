2ca9d08f7abde1a401bf5f3d17d438c7c5e1700f
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2022-04-19 22:13:37 +0200

    refactoring map and gps in market place

--- app/src/main/java/edu/polytech/repo_ihm/gps/IGPSActivity.java
+++
@@ -1,6 +0,0 @@
-package edu.polytech.repo_ihm.gps;
-
-public interface IGPSActivity {
-    int REQUEST_CODE = 400;
-    void moveCamera(); //move camera (and zoom to center the map to the gps position)
-}
--- app/src/main/java/edu/polytech/repo_ihm/gps/MapsActivity.java
+++ app/src/main/java/edu/polytech/repo_ihm/gps/MapsActivity.java
@@ -1,122 +1,181 @@
 package edu.polytech.repo_ihm.gps;
 
-import android.content.Intent;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Marker;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 import edu.polytech.repo_ihm.R;
-import edu.polytech.repo_ihm.activities.MainActivity;
-import edu.polytech.repo_ihm.activities.MarketPlaceActivity;
 
-public class MapsActivity extends FragmentActivity implements IGPSActivity, OnMapReadyCallback {
+/**
+ * Maps and GPS Activity
+ */
+public class MapsActivity extends AppCompatActivity implements LocationListener , GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
 
-    private GPSFragment gpsFragment;
-    private NavigationFragment navigationFragment;
-    GoogleMap map;
-    Button exitButton;
+    private LocationManager lm;
+    private static final int PERMS_CALL_ID = 1234;
+    private SupportMapFragment mapFragment;
+    private GoogleMap googleMap;
+    private LatLng googleLocation;
+    private Geocoder coder;
+    private boolean first = true;
     SearchView searchView;
-    private SupportMapFragment supportMapFragment;
+    private boolean sumbitText = false;
 
+
+    @SuppressLint("WrongThread")
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        assert mapFragment != null;
-        mapFragment.getMapAsync(this);
+        coder = new Geocoder(this, Locale.getDefault());
 
-        gpsFragment = (GPSFragment) getSupportFragmentManager().findFragmentById(R.id.gpsLocation);
-        if (gpsFragment == null) {
-            gpsFragment = new GPSFragment(this);
-            FragmentTransaction gpsTransaction = getSupportFragmentManager().beginTransaction();
-            gpsTransaction.replace(R.id.gpsLocation, gpsFragment);
-            gpsTransaction.addToBackStack(null);
-            gpsTransaction.commit();
-        }
-        navigationFragment = (NavigationFragment) getSupportFragmentManager().findFragmentById(R.id.navigation);
-        if (navigationFragment == null) {
-            navigationFragment = new NavigationFragment();
-            FragmentTransaction gpsTransaction = getSupportFragmentManager().beginTransaction();
-            gpsTransaction.replace(R.id.gpsLocation, navigationFragment);
-            gpsTransaction.addToBackStack(null);
-            gpsTransaction.commit();
-        }
-
-        exitButton = findViewById(R.id.back);
-        Intent intent = new Intent(MapsActivity.this, MarketPlaceActivity.class);
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(intent);
-            }
-        });
-
-        searchView = findViewById(R.id.location);
-        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        searchView = findViewById(R.id.idSearchView);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                String location = searchView.getQuery().toString();
-                List<Address> addressList =null;
-                if(location !=null || !location.equals("")){
-                    Geocoder geocoder = new Geocoder(MapsActivity.this);
-                    try {
-                        addressList =geocoder.getFromLocationName(location,1);
 
-                    }catch (IOException e){
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                sumbitText = true ;
+                List<Address> addressList = null;
+                if (location != null || location.equals("")) {
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                        System.out.println(addressList);
+
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    Address address =addressList.get(0);
-                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
-                    map.addMarker(new MarkerOptions().position(latLng).title(location));
-                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                    if(addressList!=null && addressList.size()>0){
+                        Address address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+                    }else{
+                        Toast.makeText(getApplicationContext(), "l'adresse est introuvable ", Toast.LENGTH_SHORT).show();  // Check whether the fields are not blank
+                    }
                 }
                 return false;
             }
-
             @Override
-            public boolean onQueryTextChange(String s) {
+            public boolean onQueryTextChange(String newText) {
                 return false;
             }
         });
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        checkPermission();
+        mapFragment.getMapAsync(this);
+
+
+        View locationButton = ((View) mapFragment.getView().findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        rlp.setMargins(0, 0, 30, 30);
+
+
+    }
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap mGoogleMap) {
+        this.googleMap = mGoogleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        googleMap.setMyLocationEnabled(true);
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        map = googleMap;
-        LatLng Nice =  new LatLng(50.681035, 2.224034);
-        map.addMarker(new MarkerOptions().position(Nice).title("Nice"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(Nice));
+    protected void onResume() {
+        super.onResume();
+        checkPermission();
     }
 
     @Override
-    public void moveCamera() {
-        try {
-        gpsFragment.setPlaceName("City: " + gpsFragment.getPlaceName());
-        } catch (IOException e) {
-        gpsFragment.setPlaceName("Unknown city");
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == this.PERMS_CALL_ID) {
+            checkPermission();
         }
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(gpsFragment.getPosition(), 15f));
+    }
+
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+            }, this.PERMS_CALL_ID);
+            return;
+        }
+        lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            lm.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 10000, 0, this);
+        }
+        if (lm.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
+            lm.requestLocationUpdates(
+                    LocationManager.PASSIVE_PROVIDER, 10000, 0, this);
+        }
+        if (lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            lm.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 10000, 0, this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        if (googleMap != null) {
+            googleLocation = new LatLng(latitude, longitude);
+            googleMap.setOnMarkerClickListener(this);
+            if (sumbitText == false && first == true) {
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(googleLocation, 15));
+            }
+            first = false;
+
+        }
+    }
+
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        return false;
     }
 
 }
