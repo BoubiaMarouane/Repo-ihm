93bef20b1799949e7404cc9e3879d9dd18faa693
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2022-04-27 15:46:33 +0200

    Vendre produit

--- app/src/main/java/edu/polytech/repo_ihm/MarketPlace/VendreProd.java
+++ app/src/main/java/edu/polytech/repo_ihm/MarketPlace/VendreProd.java
@@ -1,16 +1,57 @@
 package edu.polytech.repo_ihm.MarketPlace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import edu.polytech.repo_ihm.R;
+import edu.polytech.repo_ihm.activities.MarketPlaceActivity;
+import edu.polytech.repo_ihm.activities.ideeRecettes.Ingredient;
+import edu.polytech.repo_ihm.activities.ideeRecettes.IngredientAdapter;
+import edu.polytech.repo_ihm.datas.Ingredients;
+import edu.polytech.repo_ihm.datas.IngredientsAdapter;
 
 public class VendreProd extends AppCompatActivity {
+
+    ArrayList<Ingredients> productsToSell = new ArrayList<>();
+    RecyclerView recyclerView;
+
+    private IngredientsAdapter.IngredientsClickListener listener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendre_prod);
+        recyclerView = findViewById(R.id.products_to_sell);
+        setProductsToSell();
+        setAdapter();
+
+
     }
+
+    private void setAdapter() {
+        IngredientsAdapter adapter = new IngredientsAdapter(productsToSell, listener);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setProductsToSell() {
+        productsToSell.addAll(Arrays.asList(Ingredients.values()));
+    }
+
+    public void returnHomePage(View view) {
+        Intent intent = new Intent(VendreProd.this, MarketPlaceActivity.class);
+        startActivity(intent);
+    }
+
 }
